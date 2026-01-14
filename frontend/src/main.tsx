89be2b54f4e0728812fcecad1e3af1f7531f2584
Mxovela <yanela692@gmail.com>
2026-01-14 12:29:59 +0200

    finished dark mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,10 @@
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
+  import "./styles/globals.css";
   import { LoadingProvider } from "./components/ui/loading";
   import { ThemeProvider } from "./context/ThemeContext";
-
+ 
   createRoot(document.getElementById("root")!).render(
     <ThemeProvider>
       <LoadingProvider>
